f3301b20342070f5a9a42a6e483c0d39fcd0866d
William Dam <damw@oregonstate.edu>
2020-09-13 19:58:49 -0700

    Function refactor and comments.

--- cc_num_gen.py
+++ cc_num_gen.py
@@ -8,19 +8,24 @@ import random
 
 
 #######################################################################
-# Description: Helper function returns valid checksum for cc number
+# Description: Helper function returns valid checksum for cc number.
+# Uses Luhn algorithm.
 # Arguments: String of credit card number without last digit
 # Returns: Checksum for credit card number arg, as a single int
 #######################################################################
 def check_sum_digit(number):
 
-    card_digits = number
-    check_sum = 0
-    counter = 0
+    card_digits = number    # Holds incomplete card number passed from arg
+    check_sum = 0           # Holds final checksum value
+    counter = 0             # Track which digit is being calculated
 
+    # Go through each digit of number passed in, right to left
     for nums in str(number):
+
+        # Get last digit of card number
         digit = card_digits % 10
 
+        # Apply function to every other digit using counter mod 2
         if counter % 2 == 0:
             if digit * 2 > 9:
                 check_sum += (digit * 2 - 9)
@@ -29,9 +34,13 @@ def check_sum_digit(number):
         else:
             check_sum += digit
 
+        # Remove last card digit
         card_digits = int(card_digits / 10)
+
+        # Increment counter
         counter += 1
 
+    # Return check sum
     return check_sum * 9 % 10
 
 
@@ -43,11 +52,22 @@ def check_sum_digit(number):
 #######################################################################
 def random_visa():
 
+    # Set card number prefix 4
     prefix = 4
+
+    # Generate random number
     body = random.randint(00000000000000, 99999999999999)
-    num = int(str(prefix) + str(body).zfill(14))
+
+    # Concatenate leading zeros
+    body = int(str(body).zfill(14))
+
+    # Concatenate prefix
+    num = int(str(prefix) + str(body))
+
+    # Concatenate check digit
     num = int(str(num) + str(check_sum_digit(num)))
 
+    # Return complete Visa card number
     return num
 
 
@@ -59,20 +79,40 @@ def random_visa():
 #######################################################################
 def random_mc():
 
+    # Randomly choose a prefix option
     prefix_choice = random.randint(1, 2)
 
+    # Prefix [51, 55]
     if prefix_choice == 1:
+
+        # Set prefix in range
         prefix = random.randint(51, 55)
+
+        # Generate random number
         body = random.randint(0000000000000, 9999999999999)
+
+        # Concatenate leading zeros
         body = int(str(body).zfill(13))
+
+    # Prefix [2221, 2720]
     else:
+
+        # Set prefix in range
         prefix = random.randint(2221, 2720)
+
+        # Generate random number
         body = random.randint(00000000000, 99999999999)
+
+        # Concatenate leading zeros
         body = int(str(body).zfill(11))
 
+    # Concatenate prefix
     num = int(str(prefix) + str(body))
+
+    # Concatenate check digit
     num = int(str(num) + str(check_sum_digit(num)))
 
+    # Return complete MasterCard number
     return num
 
 
@@ -84,15 +124,26 @@ def random_mc():
 #######################################################################
 def random_amex():
 
+    # Randomly choose prefix choice option
     prefix_choice = random.randint(1, 2)
 
+    # Assign prefix number
     if prefix_choice == 1:
         prefix = 34
     else:
         prefix = 37
 
+    # Generate random number
     body = random.randint(000000000000, 999999999999)
+
+    # Concatenate leading zeros
     body = int(str(body).zfill(12))
 
+    # Concatenate prefix
     num = int(str(prefix) + str(body))
+
+    # Concatenate check digit
     num = int(str(num) + str(check_sum_digit(num)))
+
+    # Return complete Amex number
+    return num
---
+++ main.py
@@ -0,0 +1,7 @@
+from cc_num_gen import *
+
+print("Random Visa Number: " + str(random_visa()))
+
+print("Random MasterCard Number: " + str(random_mc()))
+
+print("Random Amex Number: " + str(random_amex()))
